9fd91d56bb10e23490878f829f07f802d40e41ac
avityagi <64332331+avityagi@users.noreply.github.com>
2022-03-28 05:54:13 +0530

    Initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.heading}</span>
+      </dt>
+      <dd>{props.data}</dd>
+    </div>
+  );
+}
+export default Entry;
